2795b4bca11a843b381ebe5c5494fd4ce45ce308
David Johansson <david.n.johansson@live.com>
2023-08-18 11:55:04 +0200

    Klar med sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, inject } from '@angular/core';
 import { ActivatedRoute, Params, RouterOutlet } from '@angular/router';
 import { ColorPaletteService } from './services/color-palette.service';
 import { CommonModule } from '@angular/common';
 import { ColorPalette } from './interfaces/color-palette.interface';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { DrawerService } from './services/drawer.service';
+import { BreakpointService } from './services/breakpoint.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -12,24 +16,39 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     RouterOutlet,
     MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  route = inject(ActivatedRoute);
-  colorPaletteService = inject(ColorPaletteService);
-  currentColorPalette: ColorPalette | null = null;
+export class AppComponent implements AfterViewInit {
+  @ViewChild(MatDrawer) public drawer!: MatDrawer;
 
-  ngOnInit() {
-    this.route.queryParams.subscribe(
-      (params: Params) => {
-        const color = params['color'];
-        if (typeof (color) !== 'string') return;
-        if (this.colorPaletteService.isValidHexColor(color) === false) return;
-        this.currentColorPalette = this.colorPaletteService.generateColorPalette(color, color);
-        console.log(this.currentColorPalette);
-      }
-    );
+  private drawerService = inject(DrawerService);
+  public breakpointService = inject(BreakpointService);
+
+  ngAfterViewInit() {
+    this.drawerService.drawer = this.drawer;
   }
+
+  public toggleDrawer() {
+    this.drawerService.drawer?.toggle();
+  }
+
+  // route = inject(ActivatedRoute);
+  // colorPaletteService = inject(ColorPaletteService);
+  // currentColorPalette: ColorPalette | null = null;
+
+  // ngOnInit() {
+  //   this.route.queryParams.subscribe(
+  //     (params: Params) => {
+  //       const color = params['color'];
+  //       if (typeof (color) !== 'string') return;
+  //       if (this.colorPaletteService.isValidHexColor(color) === false) return;
+  //       this.currentColorPalette = this.colorPaletteService.generateColorPalette(color, color);
+  //       console.log(this.currentColorPalette);
+  //     }
+  //   );
+  // }
 }
---
+++ src/app/components/color-palette/color-palette.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-color-palette',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './color-palette.component.html',
+  styleUrls: ['./color-palette.component.scss']
+})
+export class ColorPaletteComponent {
+
+}
