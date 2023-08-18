9ba72ab945013f404a7da9b3d66a9eda43ce4079
David Johansson <david.n.johansson@live.com>
2023-08-18 11:14:26 +0200

    Installerat Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,16 @@ import { ActivatedRoute, Params, RouterOutlet } from '@angular/router';
 import { ColorPaletteService } from './services/color-palette.service';
 import { CommonModule } from '@angular/common';
 import { ColorPalette } from './interfaces/color-palette.interface';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -20,7 +25,7 @@ export class AppComponent implements OnInit {
     this.route.queryParams.subscribe(
       (params: Params) => {
         const color = params['color'];
-        if (typeof(color) !== 'string') return;
+        if (typeof (color) !== 'string') return;
         if (this.colorPaletteService.isValidHexColor(color) === false) return;
         this.currentColorPalette = this.colorPaletteService.generateColorPalette(color, color);
         console.log(this.currentColorPalette);
