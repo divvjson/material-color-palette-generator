ff5a83cce63cf32446fc4717aee8c4adaf5bdd7d
David Johansson <david.n.johansson@live.com>
2023-08-19 18:24:30 +0200

    pop out effect

--- src/app/components/color-palette/color-palette.component.ts
+++ src/app/components/color-palette/color-palette.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorPaletteService } from 'src/app/services/color-palette.service';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-color-palette',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatRippleModule,
+  ],
   templateUrl: './color-palette.component.html',
   styleUrls: ['./color-palette.component.scss']
 })
