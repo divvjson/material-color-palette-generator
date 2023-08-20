dbd114aa0b9955ed2b62c7f7fcb250c2b01b1120
David Johansson <david.n.johansson@live.com>
2023-08-20 16:55:42 +0200

    Klar med info component

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss']
 })
